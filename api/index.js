a9689d6709bd8cd65d461dba271837aa5e7ad0cf
Sid Trivedi <sid.trivedi@auth0.com>
2021-01-19 03:09:42 -0800

    add scope check

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { createServer } = require("http");
-const { auth } = require("express-oauth2-bearer");
+const { auth, requiredScopes } = require("express-oauth2-bearer");
 
 const {
   checkUrl,
@@ -53,7 +53,7 @@ app.use(auth());
 // 👇 private routes below 👇
 
 
-app.get("/reports", (req, res) => {
+app.get("/reports", requiredScopes('read:reports'), (req, res) => {
   res.send(expenses);
 });
 
